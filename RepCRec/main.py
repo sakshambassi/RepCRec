4ca7e191ecd577a3407d81cac674358ceafce264
Aayush Agrawal <aayushagrawal135@gmail.com>
2022-11-29 20:57:57 -0500

    Refactor main :)

--- RepCRec/main.py
+++ RepCRec/main.py
@@ -8,32 +8,35 @@ from src.utils import log
 
 
 def main():
+    args = parse_args()
+    path = os.path.dirname(os.path.realpath(__file__))
+    configdir = "/".join([path, "config.ini"])
+    config = read_config(configdir)
+
+    if args.run_all:
+        for filename in sorted(glob.glob('tests/inputs/*')):
+            run_input(config, filename)
+    else:
+        filename = f"tests/inputs/input{args.input}"
+        run_input(config, filename)
+
+
+def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--run-all', action="store_true",
                         help='Whether to run on all tests')
     parser.add_argument("--input", default=1, type=int,
                         help="Which input test to run")
     args = parser.parse_args()
-    path = os.path.dirname(os.path.realpath(__file__))
-    configdir = "/".join([path, "config.ini"])
+    return args
 
-    config = read_config(configdir)
-    if args.run_all:
-        for filename in sorted(glob.glob('tests/inputs/*')):
-            transaction_manager = TransactionManager(
-                total_sites=int(config["CONSTANTS"]["num_sites"])
-            )
-            log(f"{'-'*100}\nProcessing for input file={filename}")
-            transaction_manager.prepare_input(filename=filename)
-    else:
-        transaction_manager = TransactionManager(
-            total_sites=int(config["CONSTANTS"]["num_sites"])
-        )
-        filename = f"tests/inputs/input{args.input}"
-        log(f"{'-'*100}\nProcessing for input file={filename}")
-        transaction_manager.prepare_input(
-            filename=filename)
 
+def run_input(config, filename: str):
+    log(f"{'-'*100}\nProcessing for input file={filename}")
+    transaction_manager = TransactionManager(
+        total_sites=int(config["CONSTANTS"]["num_sites"])
+    )
+    transaction_manager.prepare_input(filename=filename)
 
 if __name__ == "__main__":
     main()
