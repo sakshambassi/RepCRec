a1b2a49e73e3cee5eb95aa5517b86d9b9fc02528
Saksham Bassi <sakshambassi@gmail.com>
2022-12-02 17:07:48 -0500

    Add absolute path input functionality

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ To run this project all test inputs, do:
 
 To run this project on a specific test input, do:
 
-```bash run.sh <input_no>```
+```bash run.sh <filepath>```
 
 for example to run input15, do:
 
-```bash run.sh 15```
+```bash run.sh /Users/sakshambassi/Documents/ADBMS/Project/RepCRec/tests/inputs/input2```
--- RepCRec/main.py
+++ RepCRec/main.py
@@ -12,6 +12,37 @@ from src.transaction_manager import TransactionManager
 from src.utils import log
 
 
+def parse_args():
+    """ parses command line argument
+
+    Returns:
+        args: parsed command line arguments
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--run-all', action="store_true",
+                        help='Whether to run on all tests')
+    parser.add_argument("--input", type=str,
+                        help="Provide absolute path of input file")
+    args = parser.parse_args()
+    return args
+
+
+def run_input(config, filename: str):
+    """ processes given file details
+
+    Args:
+        config: config variables
+        filename (str): absolute path of input file
+
+    Returns:
+        None
+    """
+    log(f"{'-'*100}\nProcessing for input file={filename}")
+    transaction_manager = TransactionManager(
+        total_sites=int(config["CONSTANTS"]["num_sites"])
+    )
+    transaction_manager.prepare_input(filename=filename)
+
 def main():
     args = parse_args()
     path = os.path.dirname(os.path.realpath(__file__))
@@ -22,26 +53,8 @@ def main():
         for filename in sorted(glob.glob('tests/inputs/*')):
             run_input(config, filename)
     else:
-        filename = f"tests/inputs/input{args.input}"
+        filename = args.input
         run_input(config, filename)
-
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--run-all', action="store_true",
-                        help='Whether to run on all tests')
-    parser.add_argument("--input", default=1, type=int,
-                        help="Which input test to run")
-    args = parser.parse_args()
-    return args
-
-
-def run_input(config, filename: str):
-    log(f"{'-'*100}\nProcessing for input file={filename}")
-    transaction_manager = TransactionManager(
-        total_sites=int(config["CONSTANTS"]["num_sites"])
-    )
-    transaction_manager.prepare_input(filename=filename)
 
 if __name__ == "__main__":
     main()
